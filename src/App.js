663ac26060e8fd21087a23d14f9e33e335340d4c
Stanislav Iliashchuk <stanislav.iliashchuk@eleks.com>
2020-12-04 21:23:23 +0200

    hltb + simple fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { HowLongToBeatService } from 'howlongtobeat';
+
+const hltb = new HowLongToBeatService();
+
+const fetchAndLogWitcher = async () => {
+  const withcerResults = await hltb.search('Witcher 3');
+  console.log(withcerResults);
+};
 
 function App() {
+  fetchAndLogWitcher();
   return (
     <div className="App">
       <header className="App-header">
@@ -9,12 +18,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
