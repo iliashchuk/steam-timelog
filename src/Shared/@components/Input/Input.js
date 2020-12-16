833bdd3e9f1cd8fcdcedfb232e3d779ca154a342
blank-666 <71154969+blank-666@users.noreply.github.com>
2020-12-16 22:59:07 +0200

    Update src/Shared/@components/Input/Input.js
    Co-authored-by: Stanislav Iliashchuk <52495853+iliashchuk@users.noreply.github.com>

--- src/Shared/@components/Input/Input.js
+++ src/Shared/@components/Input/Input.js
@@ -3,7 +3,7 @@ import s from './Input.module.scss';
 
 export const Input = props => {
   return (
-    <div className={s.root}>
+    <div className={`${s.root} {className}`}>
       <label htmlFor={props.name}>{props.label}</label>
       <input type="text" name={props.name} value={props.value} onChange={props.handleChange}></input>
     </div>
